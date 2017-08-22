23c89f2e42c559e9c92b1620aa9e50754deb7dae
Nick Cuthbert <nick@cuthbert.co.za>
2017-08-22 08:31:41 +0200

    Ping pong is working

--- index.js
+++ index.js
@@ -3,26 +3,15 @@ const start = require('./lib/index').start;
 let system = start({ 'console.log': (actor, msg) => console.log('A' + msg)});
 
 let pongActor = system.spawn(() => function f(ctx, msg) {
-    switch (msg.payload.type) {
-        case 'PING': {
-            // console.log('PONG');
-            tell(ctx.sender, { type: 'PONG' });
-            // tell(ctx.children['alex'], 'ALEX!!', ctx.self);
-            break;
-        }
-            
-        // case 'SPWN': {
-        //     spawn(() => function f(ctx, msg) { ctx.log(msg.payload); return f; }, 'alex');
-        //     break;
-        // }
-    }
+    console.log('PING');       
+    tell(ctx.sender, { type: 'PONG' });                
     return f;
 }, 'pong');
 
 // pongActor.tell({ type: 'SPWN' });
 
 let pingActor = system.spawn(() => function f(ctx, msg) {
-    // console.log('PING');
+    console.log('PONG');
     tell(ctx.sender, { type: 'PING' });
     return f;
 }, 'ping');
