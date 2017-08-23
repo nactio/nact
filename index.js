86b6527ea051efaa2bde2a2153a33d2e9af501eb
Nick Cuthbert <nick@cuthbert.co.za>
2017-08-24 01:14:03 +0200

    Thingz

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const start = require('./lib/index').start;
-let system = start({ 
-    'console.log': { f: (actor, msg) => console.log('A: '+msg) },
-    'test':  { f: (actor) => { console.log('calling test');  return 2;}, async: true }
+
+
+let system = start({
+    'console.log': { f: (actor, msg) => console.log('A: ' + msg) },
+    'test': { f: (actor) => Promise.resolve(2), async: true }
 });
 
 let pongActor = system.spawnSimple((ctx, msg) => {
-    console.log(msg);       
-    return test().then(result=>{
-        console.log(result);        
-        tell(ctx.sender, 'PONG');                    
-    });    
+    console.log(msg);
+    
+    return test().then(result => {
+        console.log(result);
+        tell(ctx.sender, 'PONG');
+    });
+    
 }, 'pong');
 
 let pingActor = system.spawnSimple((ctx, msg) => {
     console.log(msg);
-    tell(ctx.sender, 'PING');    
+    tell(ctx.sender, 'PING');
 }, 'ping');
 
 pingActor.tell('PONG', pongActor.context.self);
