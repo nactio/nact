30bfeb18b78325945e9b202c2631fa08ae0e63bc
Nick Cuthbert <nick@cuthbert.co.za>
2017-08-24 00:06:25 +0200

    Yeah

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const start = require('./lib/index').start;
-let system = start({ 'console.log': (actor, msg) => console.log(msg)});
+let system = start({ 
+    'console.log': { f: (actor, msg) => console.log('A: '+msg) },
+    'test':  { f: (actor) => { console.log('calling test');  return 2;}, async: true }
+});
 
 let pongActor = system.spawnSimple((ctx, msg) => {
     console.log(msg);       
-    tell(ctx.sender, 'PONG');                    
+    return test().then(result=>{
+        console.log(result);        
+        tell(ctx.sender, 'PONG');                    
+    });    
 }, 'pong');
 
 let pingActor = system.spawnSimple((ctx, msg) => {
