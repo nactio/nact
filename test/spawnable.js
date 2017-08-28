ba72ffbcbf164d177a0749f6eab9d56ff0c6c47c
Nick Cuthbert <nick@cuthbert.co.za>
2017-08-29 00:15:29 +0200

    First unit test after refactor

--- test/spawnable.js
+++ test/spawnable.js
@@ -7,19 +7,37 @@ describe('Spawnable', function () {
   let system = start();
 
   describe('#spawn()', function () {
-    it('Should correctly register children when spawned', async function () {
-      let child = system.spawnSimple(() => (msg) => { tell(sender, children); }, 'testChildActor');
-      system.children.should.have.key('testChildActor');      
-      let children = await child.ask();      
-      console.log(JSON.stringify(children));
-      Object.keys(children).should.have.length(0);
-      let grandchild = child.spawn(() => { }, 'testGrandchildActor');
-      children = await child.ask();
-      children.should.have.key('testGrandchildActor');
-      child.stop();
-      await delay(100);
-      system.children.should.not.have.key('testChildActor');
+    it('registers and deregisters children from inside and outside the actor', async function () {
+      let child = system.spawnSimple(() => tell(sender, children), 'testChildActor');
       
+      system.children.should.have.key('testChildActor');      
+      
+      let children = await child.ask();            
+      Object.keys(children).should.have.length(0, 'actor should not have children yet');
+
+      let grandchild = child.spawn(() => { }, 'testGrandchildActor');            
+      await delay(100);
+      children = await child.ask();      
+      children.should.have.keys('testGrandchildActor');      
+      child.children.should.have.keys('testGrandchildActor');
+      
+      let grandchild2 = child.spawn(() => { }, 'testGrandchildActor2');            
+      await delay(100);
+      children = await child.ask();      
+      child.children.should.have.keys('testGrandchildActor2','testGrandchildActor');
+      children.should.have.keys('testGrandchildActor2','testGrandchildActor');      
+
+      grandchild.stop();
+      await delay(100);
+      children = await child.ask();
+      children.should.have.keys('testGrandchildActor2');            
+      child.children.should.have.keys('testGrandchildActor2');
+
+      child.stop();            
+      await delay(100);
+      grandchild.isStopped.should.be.true;
+      system.children.should.not.have.key('testChildActor');      
+
     });
 
   });
