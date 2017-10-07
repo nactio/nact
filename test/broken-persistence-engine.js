008be36240318ba0540fd177b12862c502c6c7bb
Nick Cuthbert <nick@cuthbert.co.za>
2017-10-07 15:04:47 +0200

    Stuff

---
+++ test/broken-persistence-engine.js
@@ -0,0 +1,20 @@
+const { AbstractPersistenceEngine } = require('../lib/persistence-engine');
+const { Observable } = require('rxjs');
+
+class BrokenPersistenceEngine extends AbstractPersistenceEngine {
+  constructor (events = new Map()) {
+    super();
+    this._events = events;
+  }
+
+  events (persistenceKey, offset, limit) {
+    return Observable.of((this._events[persistenceKey] || []).slice(offset, offset + limit));
+  }
+
+  persist (persistedEvent) {
+    const prev = this._events.get(persistedEvent.persistenceKey);
+    this._events.set(persistedEvent.persistenceKey, [...prev, persistedEvent]);
+  }
+}
+
+module.exports.MockPersistenceEngine = BrokenPersistenceEngine;
