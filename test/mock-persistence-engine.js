ad1cc46624580ec36402f0c9fecf969305e6c56b
Nick Cuthbert <nick@cuthbert.co.za>
2017-10-07 14:50:59 +0200

    Making progress with the persistent actor implementation

--- test/mock-persistence-engine.js
+++ test/mock-persistence-engine.js
@@ -4,16 +4,16 @@ const { Observable } = require('rxjs');
 class MockPersistenceEngine extends AbstractPersistenceEngine {
   constructor (events = new Map()) {
     super();
-    this.events = events;
+    this._events = events;
   }
 
   events (persistenceKey, offset, limit) {
-    return Observable.of(this.events[persistenceKey].slice(offset, offset + limit));
+    return Observable.of((this._events[persistenceKey] || []).slice(offset, offset + limit));
   }
 
   persist (persistedEvent) {
-    const prev = this.events.get(persistedEvent.persistenceKey);
-    this.events.set(persistedEvent.persistenceKey, [...prev, persistedEvent]);
+    const prev = this._events.get(persistedEvent.persistenceKey);
+    this._events.set(persistedEvent.persistenceKey, [...prev, persistedEvent]);
   }
 }
 
--- test/persistent-actor.js
+++ test/persistent-actor.js
@@ -4,8 +4,28 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 chai.should();
+const { MockPersistenceEngine } = require('./mock-persistence-engine');
+const { start, spawnPersistent } = require('../lib');
+
+const concatinativeFunction = (initialState) => {
+  const f = (state) => (msg, ctx) => {
+    ctx.tell(ctx.sender, state + msg);
+    return f(state + msg);
+  };
+  return () => f(initialState);
+};
 
 describe('PersistentActor', function () {
-  describe('#isValidName()', function () {
+  it('should startup normally if no previous events', function () {
+    const persistenceEngine = new MockPersistenceEngine();
+    const system = start({ persistenceEngine });
+    const actor = spawnPersistent(
+      system,
+      concatinativeFunction(''),
+      'test'
+    );
+    actor.tell('a');
+    actor.tell('b');
+    actor.ask('c').should.eventually.equal('abc');
   });
 });
