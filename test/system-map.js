a992ae5d4bdebe50c1cbf2f6c16d3f85d26efbb2
Nick Cuthbert <nick@cuthbert.co.za>
2018-01-04 22:09:32 +0200

    Making progress

---
+++ test/system-map.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+const chai = require('chai');
+chai.should();
+// const { start, spawnStateless, spawn, stop } = require('../lib');
+// const { add, find, remove, applyOrThrowIfStopped } = require('../lib/system-map');
+
+describe('#add()', function () {
+  it('should throw when functions are invoked', function () {
+
+  });
+});
