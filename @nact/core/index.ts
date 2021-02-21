caec7f41b8b5c58c0d1bfe3701eb59ec75808779
Natalie Cuthbert <natalie@cuthbert.co.za>
2021-02-21 21:01:07 +0200

    Exposed system map. That'll allow other system types to be discovered

--- @nact/core/index.ts
+++ @nact/core/index.ts
@@ -3,3 +3,4 @@ export { stop, query, dispatch } from './functions';
 export { Nobody } from './references';
 export * from './time';
 export { start } from './system';
+export { add as addSystem, remove as removeSystem } from './system-map';
